032fa0ec799366b6a6918436438556620a823c02
Oscar <oscarcej@gmail.com>
2019-01-11 18:25:38 -0500

    add GUI with lambda expresion

---
+++ src/vista/GuiWithLambdaExpression.java
@@ -0,0 +1,49 @@
+package vista;
+import javax.swing.*; // for GUI components
+import java.awt.event.*; // for action events
+import java.awt.*; // for layout managers
+import java.awt.TrayIcon.MessageType;
+import java.io.*;
+
+public class GuiWithLambdaExpression extends JFrame{
+	
+	JTextField txt = new JTextField(8);
+	JTextField txt1 = new JTextField(8);
+	JLabel lb = new JLabel("Result");
+	JButton btn = new JButton("Ok");
+
+	public GuiWithLambdaExpression() {
+		// TODO Auto-generated constructor stub
+		add(txt);
+		add(txt1);
+		add(btn);
+		add(lb);
+//---------------------Este metodo reemplaza los metodos de GuiWithAnonymousActionListener.java---------------------------		
+		
+		btn.addActionListener(ae ->
+		{
+				try {
+					int num = Integer.parseInt(txt.getText());
+					int num2 = Integer.parseInt(txt1.getText());
+					lb.setText(""+num+num2);
+					pack();
+				}
+				catch(Exception ex){
+					System.out.println("Error");
+				}
+		});
+//-------------------------------------------------------------------------------------------
+		
+		setLayout(new FlowLayout());
+		setVisible(true);
+		pack();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GuiWithLambdaExpression ob = new GuiWithLambdaExpression();
+	}
+
+}
